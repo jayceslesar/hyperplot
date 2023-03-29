5bd7bb84c657ebd27cf4f0eab6ef486d945a3946
jayceslesar <jslesar@beta.team>
2023-03-29 08:51:32 -0400

    asdf

---
+++ demo.py
@@ -0,0 +1,13 @@
+from hyperplot.plotting import HyperPlotter
+
+# plotter = HyperPlotter({"lwdaq": "lwdaq", "datamart": "datamart"}, max_level=9)
+plotter = HyperPlotter({"datamart": "datamart_month_linear"}, max_level=3)
+plotter.serve()
+
+asdf
+asdf
+asdf
+asdf
+asdf
+asdf
+asdf
